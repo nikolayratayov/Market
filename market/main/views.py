6bcfec9df5cdfaf4673757956aadbe893f87b759
nikolayratayov <99525124+nikolayratayov@users.noreply.github.com>
2022-12-21 16:30:13 +0200

    market app

--- market/main/views.py
+++ market/main/views.py
@@ -1,5 +1,4 @@
-from django.shortcuts import render, HttpResponse
-
+from django.shortcuts import render
 # Create your views here.
 
 
@@ -8,14 +7,5 @@ def homepage(request):
 
 
 def itemspage(request):
-    items = [
-        {
-            'name': 'Phone',
-            'price': '500'
-        },
-        {
-            'name': 'Laptop',
-            'price': '1000'
-        }
-    ]
-    return render(request, template_name='main/items.html', context={'items': items})
+
+    return render(request, template_name='main/items.html')
